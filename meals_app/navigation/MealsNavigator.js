9ae4b5c90d527080f28c42249634209ecf9a0afd
Ketan <ketantoo@gmail.com>
2020-02-02 16:36:44 +0530

    created bottomTabNavigator

--- meals_app/navigation/MealsNavigator.js
+++ meals_app/navigation/MealsNavigator.js
@@ -1,8 +1,11 @@
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createAppContainer } from "react-navigation";
+
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealsDetailScreen from "../screens/MealsDetailScreen";
-import { createAppContainer } from "react-navigation";
+import FavoritesScreen from "../screens/FavoritesScreen";
 import Colors from "../constans/Colors";
 
 const MealsNavigator = createStackNavigator(
@@ -26,4 +29,13 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorite: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      headerTitle: "Favorite Screen"
+    }
+  }
+});
+export default createAppContainer(MealsFavTabNavigator);
